47eecdb463fab797bc8371b942ec4563cd83ffc3
Shan Valdez <hisean.valdez@gmail.com>
2023-05-14 14:57:31 +0800

    Fix build failure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import getCurrentUser from "./actions/getCurrentUser";
-import getListings, { IListingsParams } from "./actions/getListings";
+import Container from "@/app/components/Container";
+import EmptyState from "@/app/components/EmptyState";
+import ListingCard from "@/app/components/listings/ListingCard";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getListings, { IListingsParams } from "@/app/actions/getListings";
 import ClientOnly from "./components/ClientOnly";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listings/ListingCard";
 
 interface HomeProps {
   searchParams: IListingsParams;
 }
+
+export const dynamic = "force-dynamic";
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
@@ -26,26 +29,24 @@ const Home = async ({ searchParams }: HomeProps) => {
       <Container>
         <div
           className="
-            grid-col-1 
-            grid 
+            grid
+            grid-cols-1 
             gap-8 
             pt-24 
             sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-4 
-            xl:grid-cols-5 
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
             2xl:grid-cols-6
           "
         >
-          {listings.map((listing) => {
-            return (
-              <ListingCard
-                key={listing.id}
-                currentUser={currentUser}
-                data={listing}
-              />
-            );
-          })}
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
